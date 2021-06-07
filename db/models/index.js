3cbb45afc915066cd6baed53f2817419e00cbc20
b00kbaker <caimmurphy@gmail.com>
2021-06-07 10:24:20 -0600

    Now listening but homepage error

--- db/models/index.js
+++ db/models/index.js
@@ -1,24 +1,24 @@
-const User = require('./user');
-const Post = require('./blogPost');
-const Comment = require('./comments');
-
-
-User.hasMany(Post, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE',
-});
+const User = require("./user");
+const Post = require("./blogPost");
+const Comment = require("./comments");
 
 Post.belongsTo(User, {
-  foreignKey: 'user_id'
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
 });
 
 Post.hasMany(Comment, {
-    foreignKey: "post_id",
-    onDelete: "CASADE",
+  foreignKey: 'postId',
+  onDelete: 'CASCADE'
 });
 
-Comment.belongsTo(Post, {
-    foreignKey: "post_id",
+Comment.belongsTo(User, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
 });
 
-module.exports = { User, Post, Comment };
+module.exports = {
+  User,
+  Comment,
+  Post
+};
--- server.js
+++ server.js
@@ -35,6 +35,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
+app.use(require("./controllers"));
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
