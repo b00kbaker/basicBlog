e6a45264aba96bc735a480a051372e336d6f0cdb
b00kbaker <caimmurphy@gmail.com>
2021-06-14 14:56:08 -0600

    Added get and delete to comment routes

--- controllers/api/commentsRoutes.js
+++ controllers/api/commentsRoutes.js
@@ -1,17 +1,46 @@
 const router = require("express").Router();
-
 const { Comment } = require("../../db/models");
+const withAuth = require("../../utils/auth");
+
+
+// GET (read) all comments
+router.get('/', (req, res) => {
+  Comment.findAll({})
+    .then(commentData => res.json(commentData))
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
 
 // POST (create) a new comment
-router.post('/', async (req, res) => {
-    try {
-      const commentData = Comment.create({
-        ...req.body,
-      });
-      res.status(200).json(commentData);
-    } catch (err) {
+router.post('/', withAuth, (req, res) => {
+  if (req.session){
+  Comment.create({
+     message: req.body.message,
+     user_id: req.session.user_id,
+  })
+  .then(commentData => res.json(commentData))
+   .catch (err => {
       res.status(400).json(err);
-    }
+    });
+  }
+});
+
+// DELETE comment (only the ones created by the validated user)
+router.delete('/:id', withAuth, (req, res) => {
+  Comment.destroy({
+      where: {id: req.params.id}
+    })
+      .then(commentData => {
+        if (!commentData) {
+          res.status(404).json({ message: "No comment with this id exists" });
+          return;
+        }
+        res.json(commentData);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
 });
   
 module.exports = router;
